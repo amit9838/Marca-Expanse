3933a3c1610fdbc428a3df50ff926e9655753865
Amit Chaudhary <amitchaudhry0539@gmail.com>
2022-07-17 21:14:07 +0530

    added requirements.txt to deploy on heroku

--- expensewebsite/settings.py
+++ expensewebsite/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'expensewebsite/static'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'expensewebsite/static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
